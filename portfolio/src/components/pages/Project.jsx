8491cd778a1456c3fefd2b4c159fe06489a8a061
prieta <sofiaboubion@gmail.com>
2024-02-25 18:14:03 -0800

    Updated projects section and background color

--- portfolio/src/components/pages/Project.jsx
+++ portfolio/src/components/pages/Project.jsx
@@ -5,8 +5,43 @@ import Card from 'react-bootstrap/Card';
 const projects = [
     {
         id: 1,
-        projectName: "Fist Project",
-        description: "This was a very fun project we worked on"
+        projectName: "Take a Hike",
+        description: "Build our own API using MySQL database and sequalizer. Search for a hike based on difficulty level.",
+        imgUrl:"https://res.cloudinary.com/dwjrsllb0/image/upload/v1708903380/personal/enigmatic-cliffs-38751-768457d3d919.herokuapp.com__1_gdtkbh.png",
+        deploymentUrl:"https://enigmatic-cliffs-38751-768457d3d919.herokuapp.com/",
+        gitHub: "https://github.com/ariSof/take-a-hike"
+    },
+    {
+        id: 2,
+        projectName: "Weather Dashboard",
+        description: "Current weather page, search by city. 5 day forecast. Uses OpenWeather API and Bulma CSS library",
+        imgUrl:"https://res.cloudinary.com/dwjrsllb0/image/upload/v1708901812/personal/screenshot_index.html_xdexpo.png",
+        deploymentUrl:"https://arisof.github.io/weather-app/",
+        gitHub: "https://github.com/ariSof/weather-app"
+    },
+    {
+        id: 3,
+        projectName: "Can I Afford It?",
+        description: "This project made use of existing APIs to fetch travel information based on data entered. ",
+        imgUrl:"https://res.cloudinary.com/dwjrsllb0/image/upload/v1708905392/personal/Screenshot_2024-01-10_at_1.25.50_PM_fbu6bh.png",
+        deploymentUrl:"https://sebjustseb.github.io/AffordIt-App/",
+        gitHub: "https://github.com/sebjustseb/AffordIt-App"
+    },
+    {
+        id: 4,
+        projectName: "Logo Maker",
+        description: "Create an svg logo using data entered on the command-line.",
+        imgUrl:"https://res.cloudinary.com/dwjrsllb0/image/upload/v1708905050/personal/Examples_dcafol.png",
+        deploymentUrl:"https://watch.screencastify.com/v/YoNtMKhjhpcB5r47GoUt",
+        gitHub: "https://github.com/ariSof/logo-maker"
+    },
+    {
+        id: 5,
+        projectName: "Technology Blog",
+        description: "A CMS style Technology Blog site where developers can publish a blog post and comment on other developer's posts. Built fullstack app and deployed to Heroku. It follows MVC architecture. Uses Handlebars for the front end, mySQL db, sequelize as ORM, and express-session npm package for authentication.",
+        imgUrl:"https://res.cloudinary.com/dwjrsllb0/image/upload/v1708905945/personal/post_and_comment_tmoa1w.png",
+        deploymentUrl:"https://mighty-retreat-24967-cb1040ca1720.herokuapp.com/",
+        gitHub: "https://github.com/ariSof/tech-blog"
     }
 ]
 
@@ -14,19 +49,21 @@ const projects = [
 function Project() {
     {
         return (
-            <div>
+            <div style={{display: 'flex', flexWrap: 'wrap'}} >
                 {
                     projects.map((project) => { 
                         return (
             
-                            <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card bg="dark" text="white" border="dark" style={{ width: '40%', margin: '3%' }}>
+                            <Card.Img variant="top" src={project.imgUrl} />
                             <Card.Body>
                                 <Card.Title>{`${project.projectName}`}</Card.Title>
                                 <Card.Text>
                                 {`${project.description}`}
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <div className="d-grid gap-2">
+                                <Button variant="secondary" size="lg" href={project.deploymentUrl} >Go to Project</Button>
+                                </div>
                             </Card.Body>
                             </Card>
                         );
